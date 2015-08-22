862ca2b892e265a418c114dfd5cf2e6eead67343
LukeJang <lukejang@Lukes-MacBook-Pro.local>
2015-08-22 12:28:21 +0800

    BatDrone iOS first commit: create Xcode project

---
+++ BatDrone-iOS/Light Up The Way/Light Up The Way/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Light Up The Way
+//
+//  Created by Luke Jang on 8/22/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BatDrone-iOS/Light Up The Way/Light Up The Way/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Light Up The Way
+//
+//  Created by Luke Jang on 8/22/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
